ad94fa884c79a2d50ed766c6c1a2fd173659f04a
杜修杏 <duxiuxing@foxmail.com>
2018-05-14 16:24:06 +0800

    exercise1~10代码上库

---
+++ source/exercise01.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+# 习题 1: 第一个程序
+
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+
+print '中华人民共和国'
