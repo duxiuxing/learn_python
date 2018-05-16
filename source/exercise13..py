9c5b61c576ef482fe8e9a3b74c2c2770328ca4b9
杜修杏 <duxiuxing@foxmail.com>
2018-05-17 00:36:10 +0800

    exercise12、13代码上库

--- source/exercise13..py
+++ source/exercise13..py
@@ -1,2 +1,11 @@
 # coding=utf-8
-#
+# 习题 13: 参数、解包、变量
+
+from sys import argv
+
+script, first, second, third = argv
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
